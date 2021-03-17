371848175f6150285e912de25d3ce652f68666b2
kimpenguin <kpeng@nycopportunity.nyc.gov>
2021-03-17 17:24:20 -0400

    commenting category programs list for complete program list on sitemap page

--- wp-content/themes/workingnyc/singular.php
+++ wp-content/themes/workingnyc/singular.php
@@ -12,43 +12,53 @@ $context['post'] = $post;
 if ($post->slug == 'sitemap') {
   $context['sitemap'] = true;
 
-  $programs = array();
-  $taxonomies = get_object_taxonomies('programs', 'objects');
+  /**
+   * Programs broken down by categories
+   */
+  // $programs = array();
+  // $taxonomies = get_object_taxonomies('programs', 'objects');
 
-  // get the terms in taxonomy
-  foreach ($taxonomies as $key => $tax) {
-    $term_posts = array(
-      'category' => array(
-        'taxonomy' => $tax->label,
-        'terms' => array()
-      )
-    );
+  // // get the terms in taxonomy
+  // foreach ($taxonomies as $key => $tax) {
+  //   $term_posts = array(
+  //     'category' => array(
+  //       'taxonomy' => $tax->label,
+  //       'terms' => array()
+  //     )
+  //   );
 
-    $terms = get_terms($tax->name, array(
-      'hide_empty' => false,
-    ));
+  //   $terms = get_terms($tax->name, array(
+  //     'hide_empty' => false,
+  //   ));
 
-    foreach ($terms as $i => $term) {
-      $arr = array(
-        'name' => $term->name,
-        'posts' => get_posts(
-          array(
-              'posts_per_page' => -1,
-              'post_type' => 'programs',
-              'tax_query' => array(
-                  array(
-                      'taxonomy' => $tax->name,
-                      'field' => 'term_id',
-                      'terms' => $term->term_id,
-                  )
-              )
-          )
-        )
-      );
-      array_push($term_posts['category']['terms'], $arr);
-    }
-    array_push($programs, $term_posts);
-  }
+  //   foreach ($terms as $i => $term) {
+  //     $arr = array(
+  //       'name' => $term->name,
+  //       'posts' => get_posts(
+  //         array(
+  //             'posts_per_page' => -1,
+  //             'post_type' => 'programs',
+  //             'tax_query' => array(
+  //                 array(
+  //                     'taxonomy' => $tax->name,
+  //                     'field' => 'term_id',
+  //                     'terms' => $term->term_id,
+  //                 )
+  //             )
+  //         )
+  //       )
+  //     );
+  //     array_push($term_posts['category']['terms'], $arr);
+  //   }
+  //   array_push($programs, $term_posts);
+  // }
+  $args = array(
+    'posts_per_page' => -1,
+    'post_type' => 'programs',
+    'orderby' => 'title', 
+    'order' => 'ASC'
+  );
+  $programs = get_posts($args);
   $context['programs'] = $programs;
 }
 
