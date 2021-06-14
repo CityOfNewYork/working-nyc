d2d677ec2a62942c219f08117022511238187896
devowhippit <devonhirth@gmail.com>
2021-06-14 14:07:20 -0400

    Move site into Timber Posts directory

--- wp-content/themes/workingnyc/functions.php
+++ wp-content/themes/workingnyc/functions.php
@@ -1,49 +1,22 @@
 <?php
 
-class Site extends TimberSite {
+/**
+ * Timber Site
+ *
+ * @author NYC Opportunity
+ */
 
-  function __construct() {
-    add_theme_support( 'title-tag' );
-    add_theme_support( 'menus' );
-    add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+require_once get_stylesheet_directory() . '/timber-posts/Site.php';
 
-    parent::__construct();
-  }
-
-  /**
-   * Timber Context Object
-   */
-  function add_to_context ( $context ) {
-    $context['language_code'] = ICL_LANGUAGE_CODE;
-    $context['newsletter_link'] = ICL_LANGUAGE_CODE == 'en'? '/newsletter' : '/'.ICL_LANGUAGE_CODE.'/newsletter';
-    $context['direction'] = (ICL_LANGUAGE_CODE === 'ar' || ICL_LANGUAGE_CODE === 'ur') ? 'rtl' : 'ltr';
-    $context['site'] = $this;
-
-    $context['programs_archive'] = get_post_type_archive_link('programs');
-
-    $context['header_menu'] = new TimberMenu('header_menu');
-    $context['footer_menu'] = new TimberMenu('footer_menu');
-    $context['footer_menu_secondary'] = new TimberMenu('footer_menu_secondary');
-    $context['footer_menu_tertiary'] = new TimberMenu('footer_menu_tertiary');
-
-    // WNYC Settings
-    $context['options'] = get_fields('options');
-
-    // Icons path
-    $matches = glob(get_template_directory().'/assets/svg/icons-*');
-    $context['icons_path'] = strstr($matches[0], '/wp-content');
-
-    return $context;
-  }
-}
-
-new Site();
+new WorkingNYC\Site();
 
 /**
  * Includes
+ *
+ * @author NYC Opportunity
  */
 
-$includes_dir    = get_template_directory() .'/includes/';
+$includes_dir = get_stylesheet_directory() . '/includes/';
 $includes = preg_grep('~\.(php)$~', scandir($includes_dir));
 
 foreach ($includes as $i => $inc) {
---
+++ wp-content/themes/workingnyc/timber-posts/Site.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Site
+ *
+ * @author NYC Opportunity
+ */
+
+namespace WorkingNYC;
+
+use TimberSite;
+use TimberMenu;
+
+class Site extends TimberSite {
+
+  function __construct() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'menus' );
+    add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+
+    parent::__construct();
+  }
+
+  /**
+   * Timber Context Object
+   */
+  function add_to_context ( $context ) {
+    $context['language_code'] = ICL_LANGUAGE_CODE;
+    $context['newsletter_link'] = ICL_LANGUAGE_CODE == 'en'? '/newsletter' : '/'.ICL_LANGUAGE_CODE.'/newsletter';
+    $context['direction'] = (ICL_LANGUAGE_CODE === 'ar' || ICL_LANGUAGE_CODE === 'ur') ? 'rtl' : 'ltr';
+    $context['site'] = $this;
+
+    $context['programs_archive'] = get_post_type_archive_link('programs');
+
+    $context['header_menu'] = new TimberMenu('header_menu');
+    $context['footer_menu'] = new TimberMenu('footer_menu');
+    $context['footer_menu_secondary'] = new TimberMenu('footer_menu_secondary');
+    $context['footer_menu_tertiary'] = new TimberMenu('footer_menu_tertiary');
+
+    // WNYC Settings
+    $context['options'] = get_fields('options');
+
+    // Icons path
+    $matches = glob(get_template_directory().'/assets/svg/icons-*');
+    $context['icons_path'] = strstr($matches[0], '/wp-content');
+
+    return $context;
+  }
+}
