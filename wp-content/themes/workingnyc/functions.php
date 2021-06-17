2d1454f0ee883a337f1adc7d51088d7d518390a8
devowhippit <devonhirth@gmail.com>
2021-06-17 12:10:26 -0400

    Includes path helper in functions.php

--- wp-content/themes/workingnyc/functions.php
+++ wp-content/themes/workingnyc/functions.php
@@ -1,24 +1,27 @@
 <?php
 
 /**
+ * Get Path Helpers
+ *
+ * @author NYC Opportunity
+ */
+
+require_once get_stylesheet_directory() . '/includes/paths.php';
+
+/**
  * Timber Site
  *
  * @author NYC Opportunity
  */
 
-require_once get_stylesheet_directory() . '/timber-posts/Site.php';
+require_once WorkingNYC\timber_post('Site');
 
 new WorkingNYC\Site();
 
 /**
- * Includes
+ * Require Includes
  *
  * @author NYC Opportunity
  */
 
-$includes_dir = get_stylesheet_directory() . '/includes/';
-$includes = preg_grep('~\.(php)$~', scandir($includes_dir));
-
-foreach ($includes as $i => $inc) {
-  require_once($includes_dir. $inc);
-}
+WorkingNYC\require_includes();
