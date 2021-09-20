3fb37b7743e5f9a88e4dd25e8013ca4a770bda5a
devowhippit <devonhirth@gmail.com>
2021-09-20 12:23:59 -0400

    Use WP filter name for plugin file name

--- wp-content/mu-plugins/admin-menu.php
+++ wp-content/mu-plugins/admin-menu.php
@@ -1,14 +1,12 @@
 <?php
 
 /**
- * Plugin Name: Remove Menus
+ * Plugin Name: Admin Menu
  * Description: Removes unused menus from the WP Dashboard
  * Author: NYC Opportunity
  */
 
 add_action('admin_menu', function() {
-
-  remove_menu_page( 'edit.php' );           //Posts
-  remove_menu_page( 'edit-comments.php' );  //Comments
-
+  remove_menu_page('edit.php');           // Posts
+  remove_menu_page('edit-comments.php');  // Comments
 });
