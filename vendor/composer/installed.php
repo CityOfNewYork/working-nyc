2600355b8699728cf7818dd81f1062283aa484c0
devowhippit <devonhirth@gmail.com>
2022-05-04 16:55:46 -0400

    v1.9.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.9.2',
-        'version' => '1.9.2.0',
+        'pretty_version' => '1.9.3',
+        'version' => '1.9.3.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -119,8 +119,8 @@
             'dev_requirement' => false,
         ),
         'nyco/working-nyc' => array(
-            'pretty_version' => '1.9.2',
-            'version' => '1.9.2.0',
+            'pretty_version' => '1.9.3',
+            'version' => '1.9.3.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
