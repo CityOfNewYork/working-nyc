5029c0d1ddff008c5a45c7d40b549f8d4b21c541
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2023-07-12 16:51:13 -0400

    lint

--- wp-content/mu-plugins/register-rest-routes.php
+++ wp-content/mu-plugins/register-rest-routes.php
@@ -192,11 +192,13 @@ add_action('rest_api_init', function() {
         // the 'tax_query' argument specifies the taxonomies to filter by
         // we need an array of arrays because we could filter by multiple taxonomies
         // (e.g. location, schedule, sector)
-        $args['tax_query'] = array( array( 
-          'taxonomy' => 'locations',
-          'field' => 'slug',
-          'terms' => $parameters['locations']
-        ));
+        $args['tax_query'] = array( 
+          array(
+            'taxonomy' => 'locations',
+            'field' => 'slug',
+            'terms' => $parameters['locations']
+          )
+        );
       }
 
       // run query
