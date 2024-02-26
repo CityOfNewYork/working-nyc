4f2a749c5b0e4b6f6e5dd2c70e757c4d87a88b94
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-02-26 14:48:26 -0500

    program card logo sizes

--- wp-content/themes/workingnyc/views/employer-programs/employer-program.vue
+++ wp-content/themes/workingnyc/views/employer-programs/employer-program.vue
@@ -1,7 +1,7 @@
 <template>
   <a :href="post.link" class="no-underline text-inherit">
-    <article class="tablet:grid tablet:grid-cols-[60px_minmax(300px,1fr)] gap-x-3 p-3 rounded border border-scale-3">
-      <img class="w-[60px] tablet:w-full mb-3" v-if="post.logo" :src="post.logo.url" loading="lazy" :alt="post.logo.alt"/>
+    <article class="tablet:grid tablet:grid-cols-[80px_minmax(300px,1fr)] gap-x-3 p-3 rounded border border-scale-3">
+      <img class="w-[80px] h-[60px] object-contain object-left mb-3" v-if="post.logo" :src="post.logo.url" loading="lazy" :alt="post.logo.alt"/>
       <div v-else></div>
       <div>
         <header class="border-b border-scale-3">
