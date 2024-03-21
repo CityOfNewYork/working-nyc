2b3a3f70810f303d2b0282fc478d620ad0295405
Kinjal Patel <kinjalpatel@APLC02V52LNG8WN.local>
2024-03-21 12:03:24 -0400

    button_Changes

--- wp-content/themes/workingnyc/views/programs/program.vue
+++ wp-content/themes/workingnyc/views/programs/program.vue
@@ -2,7 +2,7 @@
   <article class="c-card">
     <header class="c-card__header">
       <span>
-        <a class="c-card__header-link" :data-js="'post-' + post.id" :href="post.context.link" :target="(post.context.external) ? '_blank' : false" :rel="(post.context.external) ? 'noopener' : false">
+        <a class="c-card__header-link pb-2 desktop:pt-3 pt-2" :data-js="'post-' + post.id" :href="post.context.link" :target="(post.context.external) ? '_blank' : false" :rel="(post.context.external) ? 'noopener' : false">
           <h3 class="c-card__title">
             <span class="c-card__underline" v-html="post.context.program_plain_language_title"></span>
 
@@ -12,7 +12,7 @@
           </h3>
         </a>
 
-        <p class="c-card__subtitle text-alt">
+        <p class="c-card__subtitle text-alt pb-2 desktop:pb-3">
           <b data-program="title" v-html="post.context.program_title"></b>
           <span v-if="post.context.program_agency"> {{ strings.BY }} </span>
           <span v-if="post.context.program_agency" v-html="post.context.program_agency"></span>
@@ -20,7 +20,7 @@
       </span>
     </header>
 
-    <div class="c-card__body">
+    <div class="c-card__body py-3 px-5 desktop:pt-3 desktop:pb-3 tablet:px-5 tablet:px-5">
       <p class="c-card__status flex items-center" v-if="post.status">
         <mark class="badge mie-2" data-program="recruiting" v-if="post.context.status.recruiting">
           {{ post.context.status.recruiting.name }}<span class="sr-only">.</span>
@@ -44,48 +44,47 @@
       </p>
 
       <div class="c-card__summary">
-        <p>
+        <p class="pb-3">
           <span v-if="post.context.preview" v-html="post.context.preview"></span>
         </p>
       </div>
 
       <ul class="c-card__features">
         <li v-if="post.context.services">
-          <svg class="icon-ui c-card__feature-icon" role="img">
+          <svg class="icon-ui c-card__feature-icon h-22 w-22" role="img">
             <title>{{ strings.SERVICES }}</title>
 
             <use href="#lucide-award"></use>
           </svg>
 
-          <span v-html="post.context.services"></span>
+          <span class="pb-1" v-html="post.context.services"></span>
         </li>
 
         <li v-if="post.context.schedule">
-          <svg class="icon-ui c-card__feature-icon" role="img">
+          <svg class="icon-ui c-card__feature-icon h-22 w-22" role="img">
             <title>{{ strings.SCHEDULE }}</title>
 
             <use href="#lucide-calendar"></use>
           </svg>
 
-          <span v-html="post.context.schedule"></span>
+          <span class="pb-3" v-html="post.context.schedule"></span>
         </li>
 
         <li v-if="post.context.supports">
-          <svg class="icon-ui c-card__feature-icon" role="img">
+          <svg class="icon-ui c-card__feature-icon h-22 w-22" role="img">
             <title>{{ strings.SUPPORTS }}</title>
 
             <use href="#lucide-heart-handshake"></use>
           </svg>
 
-          <span v-html="post.context.supports"></span>
+          <span class="pb-3" v-html="post.context.supports"></span>
         </li>
       </ul>
 
-      <a class="c-card__cta" :href="post.context.link" :target="(post.context.external) ? '_blank' : false" :rel="(post.context.external) ? 'noopener' : false">
-        <span v-if="post.context.external" v-html="post.context.link_label"></span>
-        <span v-else v-html="strings.LEARN_MORE_ABOUT.replace('{{ program }}', post.context.program_plain_language_title)"></span>
-
-        <svg aria-hidden="true" class="icon-ui rtl:flip">
+      <a class="btn btn-secondary desktop:w-fit w-full pb-2 px-3 pt-2 " :href="post.context.link" :target="(post.context.external) ? '_blank' : false" :rel="(post.context.external) ? 'noopener' : false">
+        <span class="text-[22px] font-[700]" v-if="post.context.external" v-html="post.context.link_label"></span>
+        <span class="text-[22px] font-[700]" v-else v-html="strings.LEARN_MORE_ABOUT.replace('{{ program }}', post.context.program_plain_language_title)"></span>
+        <svg aria-hidden="true" class="icon-ui rtl:flip h-22 w-22">
           <use :href="(post.context.external) ? '#lucide-external-link' : '#lucide-arrow-right'"></use>
         </svg>
       </a>
