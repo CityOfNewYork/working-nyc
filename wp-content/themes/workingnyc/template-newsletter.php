f58eec0e9da55812b38de6599cd8b0c15d61fdb9
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-05-03 10:29:00 -0400

    add new newsletter template and hardcode form fields

---
+++ wp-content/themes/workingnyc/template-newsletter.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Template Name: Newsletter
+ *
+ * @author NYC Opportunity
+ */
+
+/**
+ * Context
+ *
+ * @author NYC Opportunity
+ */
+
+$context = Timber::get_context();
+
+$post = Timber::get_post();
+
+$context['meta'] = new WorkingNYC\Meta($post);
+
+$context['post'] = $post;
+
+// Enqueue
+add_action('wp_enqueue_scripts', function() {
+  enqueue_script('newsletter');
+});
+
+$template = 'newsletter-old.twig';
+
+$context['show_newsletter'] = false;
+
+$context['form_fields'] = WorkingNYC\parse_fields($post->content);
+
+// Populated email from newsletter object
+if (isset($_REQUEST['EMAIL'])) {
+  if ($_REQUEST['EMAIL'] != "") {
+    $context['email'] = htmlspecialchars($_REQUEST['EMAIL']);
+    $context['newsletter_message'] = true;
+  }
+
+  unset($_REQUEST['EMAIL']);
+} else {
+  $context['newsletter_message'] = false;
+  $context['email'] = '';
+}
+
+/**
+ * Render the view
+ *
+ * @author NYC Opportunity
+ */
+
+Timber::render($template, $context);
