14e30c4b55f7ccf843d3ffa2143f8fcc83e65a80
Mithun Pandeti <mpandeti@nycopportunity.nyc.gov>
2024-05-16 14:46:39 -0400

    Updated title tags using add_filter hook

--- wp-content/themes/workingnyc/template-home-page.php
+++ wp-content/themes/workingnyc/template-home-page.php
@@ -40,6 +40,14 @@ $context['announcements'] = array_map(function($post) {
   'order' => 'ASC',
 )));
 
+$current_page_title = $post->page_title;
+
+if(!empty($current_page_title)){
+  add_filter('document_title',function() use ( $current_page_title ) { 
+    return $current_page_title;
+  },10,1);
+}
+
 /**
  * Set Meta context
  */
