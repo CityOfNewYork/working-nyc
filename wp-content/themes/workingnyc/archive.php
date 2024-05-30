c4cf8865b43ed3cc95e1ed3f21e0a4d0be09257b
Mithun Pandeti <mithunpandeti@gmail.com>
2024-05-30 10:39:22 -0400

    Fixed php lint errors

--- wp-content/themes/workingnyc/archive.php
+++ wp-content/themes/workingnyc/archive.php
@@ -162,10 +162,10 @@ if ($path === 'employer-programs' || $path === 'services') {
 
 $current_page_title = $post->page_title;
 
-if(!empty($current_page_title)){
-  add_filter('document_title',function() use ( $current_page_title ) { 
+if (!empty($current_page_title)) {
+  add_filter('document_title', function() use ($current_page_title) {
     return esc_html($current_page_title);
-  },10,1);
+  }, 10, 1);
 }
 
 /**
