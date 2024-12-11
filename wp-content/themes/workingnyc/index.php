c42ef2c6245450c972955a8ea8839d8f01a72c74
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-12-11 12:56:12 -0500

    redirect 404 to homepage

--- wp-content/themes/workingnyc/index.php
+++ wp-content/themes/workingnyc/index.php
@@ -7,30 +7,5 @@
  *
  */
 
-// Use the landing page template but with the homepage
-
-/**
- * Context
- */
-
- $context = Timber::get_context();
-
- $post = Timber::get_post(get_option('page_on_front'));
- 
- $context['post'] = $post;
- 
- $context['hide_jobseeker_and_employer_navigation'] = true;
- 
- /**
-  * Set Meta context
-  */
- 
- $context['meta'] = new WorkingNYC\Meta($post);
- 
- /**
-  * Render the view
-  */
- 
- $compiled = new WorkingNYC\CompileImgPreload('talent-portal-landing-page.twig', $context);
- 
- echo $compiled->html;
+// redirect to homepage
+wp_safe_redirect(home_url());
